021a6a6b5617c804222ee4a64f0cf4f76d538d7d
ManeeshChintalapudi12 <chintalapudimaneesh@gmail.com>
2025-05-09 15:38:08 -0500

    Applied Material UI components to Change Inputs,Labels,Radio-Button and CheckBox

--- src/App.js
+++ src/App.js
@@ -1,150 +1,114 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  Container, Paper, Typography, TextField, Button, FormControl, FormLabel,
+  RadioGroup, FormControlLabel, Radio, FormGroup, Checkbox, Select, MenuItem
+} from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <form className="container">
-        <h1>Patient Registration Form</h1>
-        <p>Thank you for applying to our practice. Please complete this patient registration form with your information, and a doctor will contact you shortly.</p>
+    <Container maxWidth="md">
+      <Paper elevation={3} className="mui-form-container">
+        <Typography variant="h4" gutterBottom>Patient Registration Form</Typography>
+        <Typography variant="body1" gutterBottom>
+          Thank you for applying to our practice. Please complete this patient registration form with your information, and a doctor will contact you shortly.
+        </Typography>
         <hr />
 
+        <form noValidate autoComplete="off">
 
-      <div className="form-row">
-        <label htmlFor="fname">Patient's Name*</label>
-        <div className="input-group">
-          <input type="text" id="fname" name="first" placeholder="First" required />
-          <input type="text" id="lname" name="last" placeholder="Last" required />
-        </div>
-      </div>
-
-      <div className="form-row">
-        <label>Gender</label>
-        <div className="gender">
-          <label><input type="radio" name="gender" value="male" /> Male</label>
-          <label><input type="radio" name="gender" value="female" /> Female</label>
-        </div>
-      </div>
-
-      <div className="form-row">
-        <label htmlFor="Phone">Phone*</label>
-        <div className="input-group">
-          <input type="tel" id="Phone" name="Phone" placeholder="### ### ####" required />
-        </div>
-      </div>
-
-      <div className="form-row">
-        <label htmlFor="DOB">Date of Birth*</label>
-        <div className="input-group">
-          <input type="date" className="dateofbirth" id="DOB" name="dob" placeholder="MM/DD/YYYY" required />
-        </div>
-      </div>
-
-      <div className="form-row">
-        <label>Marital Status*</label>
-        <div className="status">
-          <label>
-        <input type="radio" name="MStatus" value="Single" required /> Single
-          </label>
-          <label>
-            <input type="radio" name="MStatus" value="Married" /> Married
-          </label>
-          <label>
-            <input type="radio" name="MStatus" value="Divorced" /> Divorced
-          </label>
-          <label>
-            <input type="radio" name="MStatus" value="Widow" /> Widow
-          </label>
-        </div>
-      </div>
-
-      <div className="form-row-address">
-        <label htmlFor="Address">Patient's Address*</label>
-        <div className="address">
-          <input
-            type="text"
-            className="Street-Address"
-            id="Address"
-            name="street_address"
-            placeholder="Street Address"
-            required
-          />
-
-          <div className="input-group">
-            <input type="text" className="city-address" name="city" placeholder="City" required />
-            <input type="text" className="state-address" name="state" placeholder="State" required />
+          <div className="form-row-inline">
+            <FormLabel className="form-label">Patient's Name*</FormLabel>
+            <TextField label="First" name="first" required className="form-input" />
+            <TextField label="Last" name="last" required className="form-input" />
           </div>
 
-          <div className="input-group">
-            <input type="text" className="zip-address" name="zip" placeholder="Postal/Zip Code" required />
-            <select className="country-dropdown" name="country" required>
-              <option value="">Country</option>
-              <option value="us">United States</option>
-              <option value="ca">Canada</option>
-              <option value="uk">United Kingdom</option>
-              <option value="au">Australia</option>
-              <option value="in">India</option>
-              <option value="de">Germany</option>
-              <option value="fr">France</option>
-              <option value="jp">Japan</option>
-              <option value="cn">China</option>
-            </select>
+
+          <div className="form-row-inline">
+            <FormLabel className="form-label">Gender</FormLabel>
+            <RadioGroup row name="gender">
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel value="female" control={<Radio />} label="Female" />
+            </RadioGroup>
           </div>
-        </div>
-      </div>
 
-      <div className="form-row">
-        <label htmlFor="Iname">Insurance Name*</label>
-        <div className="input-group">
-          <input type="text" id="Iname" name="insurance_name" required />
-        </div>
-      </div>
 
-      <div className="form-row-medical">
-        <label>Past Medical History*</label>
-        <div className="medical">
-          <label>
-            <input type="checkbox" name="medical_history" value="Anemia" /> Anemia
-          </label>
-          <label>
-            <input type="checkbox" name="medical_history" value="Asthma" /> Asthma
-          </label>
-          <label>
-            <input type="checkbox" name="medical_history" value="Bronchitis" /> Bronchitis
-          </label>
-          <label>
-            <input type="checkbox" name="medical_history" value="Chickenpox" /> Chickenpox
-          </label>
-          <label>
-            <input type="checkbox" name="medical_history" value="Diabetes" /> Diabetes
-          </label>
-          <label>
-            <input type="checkbox" name="medical_history" value="Pneumonia" /> Pneumonia
-          </label>
-          <label>
-            <input type="checkbox" name="medical_history" value="Thyroid Disease" /> Thyroid Disease
-          </label>
-          <label>
-            <input type="checkbox" name="medical_history" value="Ulcer" /> Ulcer
-          </label>
-          <label>
-            <input type="checkbox" name="medical_history" value="Other" /> Other
-          </label>
-        </div>
-      </div>
+          <div className="form-row-inline">
+            <FormLabel className="form-label">Phone*</FormLabel>
+            <TextField label="### ### ###" name="phone" required className="form-phone" />
+          </div>
 
-      <h2>Patient/Guardian Signature*</h2>
-      <p>
-        According to our privacy policy and federal law, your information within this patient registration form will
-        remain private at all times.
-      </p>
+          <div className="form-row-inline">
+            <FormLabel className="form-label">Date of Birth*</FormLabel>
+            <TextField  type="date"  name="dob" required className="form-gender" />
+          </div>
 
-      <div className="Register-Button">
-        <button type="submit" className="button">REGISTER</button>
-      </div>
-     </form> 
+          <div className="form-row-inline">
+            <FormLabel className="form-label">Marital Status*</FormLabel>
+            <RadioGroup row name="MStatus">
+              <FormControlLabel value="Single" control={<Radio />} label="Single" />
+              <FormControlLabel value="Married" control={<Radio />} label="Married" />
+              <FormControlLabel value="Divorced" control={<Radio />} label="Divorced" />
+              <FormControlLabel value="Widow" control={<Radio />} label="Widow" />
+            </RadioGroup>
+          </div>
 
-    </div>
+          <div className="form-row-inline">
+            <FormLabel className="form-label">Street Address*</FormLabel>
+            <TextField label="Street" name="street_address" required fullWidth className="form-input" />
+          </div>
+
+          <div className="form-row-inline">
+            <FormLabel className="form-label">City/State*</FormLabel>
+            <TextField label="City" name="city" required className="form-input" />
+            <TextField label="State" name="state" required className="form-input" />
+          </div>
+
+          <div className="form-row-inline">
+            <FormLabel className="form-label">Zip/Country*</FormLabel>
+            <TextField label="Postal/Zip Code" name="zip" required className="form-input" />
+            <FormControl label="Country" required className="form-input">
+              <Select defaultValue="">
+                <MenuItem value="us">United States</MenuItem>
+                <MenuItem value="ca">Canada</MenuItem>
+                <MenuItem value="uk">United Kingdom</MenuItem>
+                <MenuItem value="au">Australia</MenuItem>
+                <MenuItem value="in">India</MenuItem>
+                <MenuItem value="de">Germany</MenuItem>
+                <MenuItem value="fr">France</MenuItem>
+                <MenuItem value="jp">Japan</MenuItem>
+                <MenuItem value="cn">China</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+
+
+          <div className="form-row-inline">
+            <FormLabel className="form-label">Insurance Name*</FormLabel>
+            <TextField name="insurance_name" required className="form-insurance" />
+          </div>
+
+          <div className="form-row-inline medical-history-row">
+            <FormLabel className="form-label">Past Medical History*</FormLabel>
+            <FormGroup className="medical-checkbox-group">
+              {['Anemia', 'Asthma', 'Bronchitis', 'Chickenpox', 'Diabetes', 'Pneumonia', 'Thyroid Disease', 'Ulcer', 'Other'].map(item => (
+                <FormControlLabel control={<Checkbox />} label={item} key={item} />
+              ))}
+            </FormGroup>
+          </div>
+
+          <Typography variant="h6" gutterBottom>Patient/Guardian Signature*</Typography>
+          <Typography variant="body1" gutterBottom>
+            According to our privacy policy and federal law, your information within this patient registration form will remain private at all times.
+          </Typography>
+
+          <Button type="submit" variant="contained" fullWidth className="mui-submit-button">
+            REGISTER
+          </Button>
+
+        </form>
+      </Paper>
+    </Container>
   );
 }
 
