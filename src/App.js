44d0833646fecdec1653995ef85f0255ceb8d03b
ManeeshChintalapudi12 <chintalapudimaneesh@gmail.com>
2025-05-20 09:36:42 -0500

    Created a link to connect with BackEnd

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
     };
 
     try {
-      const response = await fetch('http://mc-patient-registration-service.azurewebsites.net/api/Patient/Register', {
+      const response = await fetch('https://mc-patient-registration-service.azurewebsites.net/api/Patient/Register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
